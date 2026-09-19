d952224e355dfc7b659acc4233c1b47c05a15b41
TANAALA VIMAN VALLI <viman956@gmail.com>
2026-09-19 14:07:00 +0530

    changed crocss

--- backend/main.py
+++ backend/main.py
@@ -33,20 +33,16 @@ FRONTEND_URL = os.getenv(
 
 app.add_middleware(
     CORSMiddleware,
-
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://ytube-1.onrender.com",
         FRONTEND_URL,
     ],
-
     allow_credentials=True,
-
     allow_methods=["*"],
-
     allow_headers=["*"],
 )
-
 
 # =========================================
 # DOWNLOAD FOLDER
